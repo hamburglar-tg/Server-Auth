101eaa317340651ad97a4095af73147146b80ac0
dfgfdgfm <g.nikita034@gmail.com>
2023-10-10 12:39:46 +0300

    newauth
    newauth

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById('login').onclick = function() {
+    
+    document.getElementById('window-reg').style.display = 'none';
+    document.getElementById('window-log').style.display = 'block';
+};
+document.getElementById('sign-up').onclick = function() {
+    
+    document.getElementById('register-menu').style.display = 'block';
+    document.getElementById('login-menu').style.display = 'none';
+};
